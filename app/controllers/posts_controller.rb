d020f31b898bda80d8e885a7e5fe84703bfbba62
heiniumiao <baiyanhong0321@gmail.com>
2017-11-03 22:04:16 +0300

    add order by created_at to group's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:group_id])
+    @posts = @group.posts
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @group.destroy
+    redirect_to group_path
+    flash[:alert] = "Post deleted"
+  end
+
+  private
+
+  def  post_params
+    params.require(:post).permit(:content)
+  end
+
+
 end
